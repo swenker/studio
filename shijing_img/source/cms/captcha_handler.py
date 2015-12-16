ff0aa597244250117b3a7ab26a0aee0429d794c7
wenjusun <wenjusun@motorola.com>
2015-12-16 15:29:22 +0800

    replace homepage image slider

--- shijing_img/source/cms/captcha_handler.py
+++ shijing_img/source/cms/captcha_handler.py
@@ -3,8 +3,9 @@ __author__ = 'wenjusun'
 
 from io import BytesIO
 from captcha.image import ImageCaptcha
+import random
 
-image = ImageCaptcha(fonts=['/home/wenjusun/studio/studio/shijing_img/conf/Abyssinica_SIL.ttf', '/usr/share/fonts/vlgothic/VL-Gothic-Regular.ttf'])
+image = ImageCaptcha(fonts=['/var/shijing/Abyssinica_SIL.ttf', '/usr/share/fonts/vlgothic/VL-Gothic-Regular.ttf'])
 
 data = image.generate('1234')
 #assert isinstance(data, BytesIO)
@@ -22,4 +23,8 @@ class CaptchImage():
 ch_az=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
        '0','1','2','3','4','5','6','7','8','9']
+
 def get_random_characters(width=4):
+
+    return random.sample(ch_az,width)
+
